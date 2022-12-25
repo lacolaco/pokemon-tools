0fa1a975eadaf15c7ebbdde9f13fb3391d416b19
Suguru Inatomi <suguru.inatomi@gmail.com>
2022-12-25 18:49:42 +0900

    style: add font

--- projects/app/src/main.ts
+++ projects/app/src/main.ts
@@ -2,7 +2,8 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app-routing';
 import { AppComponent } from './app/app.component';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 bootstrapApplication(AppComponent, {
-  providers: [provideRouter(routes)],
+  providers: [provideAnimations(), provideRouter(routes)],
 }).catch((err) => console.error(err));
