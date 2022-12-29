fefd8c43d48d877f7da9fbf2b766fe0290b069af
Suguru Inatomi <suguru.inatomi@gmail.com>
2022-12-29 23:32:32 +0900

    fix: og meta

--- projects/app/src/app/app.component.ts
+++ projects/app/src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Location } from '@angular/common';
+import { Component, inject, OnInit } from '@angular/core';
+import { Meta } from '@angular/platform-browser';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from './footer/footer.component';
 import { StatsPageComponent } from './stats/stats.component';
@@ -18,4 +20,14 @@ import { StatsPageComponent } from './stats/stats.component';
   styleUrls: ['./app.component.scss'],
   imports: [RouterOutlet, StatsPageComponent, FooterComponent],
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  readonly loc = inject(Location);
+  readonly meta = inject(Meta);
+
+  ngOnInit(): void {
+    this.loc.onUrlChange(() => {
+      this.meta.updateTag({ name: 'og:title', content: document.title });
+      this.meta.updateTag({ property: 'og:url', content: location.href });
+    });
+  }
+}
