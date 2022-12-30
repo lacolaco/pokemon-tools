301f69c23250c95166630baa46c8bf00b622343a
Suguru Inatomi <suguru.inatomi@gmail.com>
2022-12-30 20:03:20 +0900

    fix: add firebase analytics

---
+++ projects/app/src/config/firebase.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyDSsCaTORJvIH0V1U10iXtXINHiZRrKHn4',
+  authDomain: 'pokemon-tools-1e175.firebaseapp.com',
+  projectId: 'pokemon-tools-1e175',
+  storageBucket: 'pokemon-tools-1e175.appspot.com',
+  messagingSenderId: '39289752392',
+  appId: '1:39289752392:web:ef672b55deec801d5e817d',
+  measurementId: 'G-B5QNV8JTNE',
+};
--- projects/app/src/main.ts
+++ projects/app/src/main.ts
@@ -1,9 +1,21 @@
+import { importProvidersFrom } from '@angular/core';
+import { getAnalytics, provideAnalytics, ScreenTrackingService } from '@angular/fire/analytics';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getPerformance, providePerformance } from '@angular/fire/performance';
 import { bootstrapApplication } from '@angular/platform-browser';
+import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app-routing';
 import { AppComponent } from './app/app.component';
-import { provideAnimations } from '@angular/platform-browser/animations';
+import { firebaseConfig } from './config/firebase';
 
 bootstrapApplication(AppComponent, {
-  providers: [provideAnimations(), provideRouter(routes)],
+  providers: [
+    provideAnimations(),
+    provideRouter(routes),
+    importProvidersFrom(provideFirebaseApp(() => initializeApp(firebaseConfig))),
+    importProvidersFrom(provideAnalytics(() => getAnalytics())),
+    ScreenTrackingService,
+    importProvidersFrom(providePerformance(() => getPerformance())),
+  ],
 }).catch((err) => console.error(err));
