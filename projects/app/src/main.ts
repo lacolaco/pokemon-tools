ad377cc4865b6cf2f0089902e0cdf807435aad78
Suguru Inatomi <suguru.inatomi@gmail.com>
2022-12-31 10:22:23 +0900

    fix: inject firebase services

--- projects/app/src/main.ts
+++ projects/app/src/main.ts
@@ -1,7 +1,7 @@
-import { importProvidersFrom } from '@angular/core';
+import { APP_INITIALIZER, importProvidersFrom, inject } from '@angular/core';
 import { getAnalytics, provideAnalytics, ScreenTrackingService } from '@angular/fire/analytics';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
-import { getPerformance, providePerformance } from '@angular/fire/performance';
+import { getPerformance, providePerformance, Performance } from '@angular/fire/performance';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideRouter } from '@angular/router';
@@ -17,5 +17,16 @@ bootstrapApplication(AppComponent, {
     importProvidersFrom(provideAnalytics(() => getAnalytics())),
     ScreenTrackingService,
     importProvidersFrom(providePerformance(() => getPerformance())),
+    {
+      provide: APP_INITIALIZER,
+      useFactory: () => {
+        // start performance monitoring
+        inject(Performance);
+        // start screen tracking
+        inject(ScreenTrackingService);
+        return () => true;
+      },
+      multi: true,
+    },
   ],
 }).catch((err) => console.error(err));
