b6dca2ed09c3a8b8989bc1364f3b57cefcdb3e2a
Suguru Inatomi <suguru.inatomi@gmail.com>
2023-09-06 11:46:05 +0900

    fix: use esbuild and optimized image

--- projects/app/src/app/shared/pokemon-sprite.component.ts
+++ projects/app/src/app/shared/pokemon-sprite.component.ts
@@ -1,3 +1,4 @@
+import { NgOptimizedImage } from '@angular/common';
 import { ChangeDetectionStrategy, Component, inject, Input } from '@angular/core';
 import type { Pokemon } from '@lacolaco/pokemon-data';
 import { PokemonData } from './pokemon-data';
@@ -5,20 +6,18 @@ import { PokemonData } from './pokemon-data';
 @Component({
   selector: 'pokemon-sprite',
   standalone: true,
-  template: ``,
+  imports: [NgOptimizedImage],
+  template: `<img [ngSrc]="src" [width]="size" [height]="size" [alt]="pokemon.name" />`,
   styles: [
     `
-      :host {
-        display: block;
+      img {
         aspect-ratio: 1/1;
-        background-size: contain;
-        background-repeat: no-repeat;
         margin-top: -10%;
       }
     `,
   ],
   host: {
-    '[style.background-image]': '"url(" + src + ")"',
+    class: 'block',
   },
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
@@ -26,6 +25,8 @@ export class PokemonSpriteComponent {
   private readonly pokemonData = inject(PokemonData);
 
   @Input() pokemon!: Pokemon;
+
+  @Input() size = 32;
 
   get src() {
     return this.pokemonData.getSpriteURL(this.pokemon);
